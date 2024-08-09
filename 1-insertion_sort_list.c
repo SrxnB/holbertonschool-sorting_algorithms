870f244547a314ca596137f3cc506900a03c71e0
SrxnB <serxan.bagirli@gmail.com>
2024-08-09 20:46:07 +0000

    1-insertion_sort_list.c

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers
+ *
+ * @list: double link list head
+ *
+ * Return: No Return
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current;
+	listint_t *tmp;
+
+	current = *list;
+	current = current->next;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+	while (current != NULL)
+	{
+		while (current->prev != NULL && current->n < current->prev->n)
+		{
+			tmp = current;
+
+			if (current->next != NULL)
+				current->next->prev = tmp->prev;
+			current->prev->next = tmp->next;
+			current = current->prev;
+			tmp->prev = current->prev;
+			tmp->next = current;
+			if (current->prev != NULL)
+				current->prev->next = tmp;
+			current->prev = tmp;
+
+			if (tmp->prev == NULL)
+				*list = tmp;
+
+			print_list(*list);
+			current = current->prev;
+		}
+		current = current->next;
+	}
+}
